087d272160a519dbced3bbae9a55369a2772187e
IsraelIyke <nwangwuisrael@gmail.com>
2024-02-28 22:26:38 +0100

    clipboard Tutorial done

--- app/page.js
+++ app/page.js
@@ -1,9 +1,49 @@
+"use client";
+import ClipboardJS from "clipboard";
+import { useEffect, useRef, useState } from "react";
+
 export default function Home() {
+  const [generate, setGenerate] = useState();
+  const [notify, setNotify] = useState(false);
+  const copy = useRef(null);
+
+  useEffect(() => {
+    const clipboard = new ClipboardJS(copy.current);
+
+    clipboard.on("success", function (e) {
+      notification();
+      setTimeout(() => {
+        setNotify((prev) => !prev);
+      }, 3000);
+      e.clearSelection();
+    });
+
+    clipboard.on("error", function (e) {
+      console.error("Action:", e.action);
+    });
+    return () => {
+      clipboard.destroy();
+    };
+  }, []);
+
+  function notification() {
+    setNotify((prev) => !prev);
+  }
   return (
     <div className="container">
-      <button className="generate-btn">generate</button>
-      <p className="generate">my text</p>
-      <button className="copy-btn">copy</button>
+      <button
+        ref={copy}
+        className="generate-btn"
+        onClick={() => setGenerate(Math.random())}
+      >
+        generate
+      </button>
+      <p className="generate" id="foo">
+        {generate}
+      </p>
+      <button ref={copy} className="copy-btn" data-clipboard-target="#foo">
+        {notify ? "copied" : "copy"}
+      </button>
     </div>
   );
 }
