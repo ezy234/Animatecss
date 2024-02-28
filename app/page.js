c032ed60f772f7d85bf24c282644c65701d0dfdf
IsraelIyke <nwangwuisrael@gmail.com>
2024-02-28 21:01:08 +0100

    ok

--- app/page.js
+++ app/page.js
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <div className=""></div>;
+  return (
+    <div className="container">
+      <button className="generate-btn">generate</button>
+      <p className="generate">my text</p>
+      <button className="copy-btn">copy</button>
+    </div>
+  );
 }
