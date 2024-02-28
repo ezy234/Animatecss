160e862d31a3ce584a1e142635caf651d68e5c02
ReactNextLib <146588518+ezy234@users.noreply.github.com>
2024-02-28 22:39:43 +0100

    hot fix

--- app/page.js
+++ app/page.js
@@ -1,49 +1,20 @@
-"use client";
-import ClipboardJS from "clipboard";
-import { useEffect, useRef, useState } from "react";
+import "animate.css";
 
 export default function Home() {
-  const [generate, setGenerate] = useState();
-  const [notify, setNotify] = useState(false);
-  const copy = useRef(null);
-
-  useEffect(() => {
-    const clipboard = new ClipboardJS(copy.current);
-
-    clipboard.on("success", function (e) {
-      notification();
-      setTimeout(() => {
-        setNotify((prev) => !prev);
-      }, 3000);
-      e.clearSelection();
-    });
-
-    clipboard.on("error", function (e) {
-      console.error("Action:", e.action);
-    });
-    return () => {
-      clipboard.destroy();
-    };
-  }, []);
-
-  function notification() {
-    setNotify((prev) => !prev);
-  }
   return (
-    <div className="container">
-      <button
-        ref={copy}
-        className="generate-btn"
-        onClick={() => setGenerate(Math.random())}
-      >
-        generate
-      </button>
-      <p className="generate" id="foo">
-        {generate}
-      </p>
-      <button ref={copy} className="copy-btn" data-clipboard-target="#foo">
-        {notify ? "copied" : "copy"}
-      </button>
+    <div className=" flex justify-center items-center h-screen text-center overflow-hidden">
+      <div className="bg-div"></div>
+      <div className="glass w-[80vw] h-[40vh] md:w-[50vw] md:h-[40vh] animate__animated animate__lightSpeedInRight flex flex-col justify-center">
+        <h1 className="animate__animated animate__tada animate__delay-1s text-[3rem] md:text-[5rem] font-[800] text-orange-900">
+          Animate<span className=" text-black">.</span>css
+        </h1>
+        <p className="animate__animated animate__zoomInDown animate__delay-4s text-[1.2rem] font-[500] mt-[0.5rem]">
+          A cross-browser library for CSS animation
+        </p>
+        <p className="animate__animated animate__zoomInDown animate__delay-5s text-[1rem] font-[300] mt-[0.5rem]">
+          Animate without Javascript
+        </p>
+      </div>
     </div>
   );
 }
